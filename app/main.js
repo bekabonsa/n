5b9f62afa5890dce854feb68a2a9974b31ba12d2
bex <bekadanielbonsa@gmail.com>
2026-04-01 21:15:55 +0200

    Point Tizen test launcher at HTML-hosted site build

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 (function openHostedNuvioTvBuild() {
-  var launcherBuild = "2026-04-01b";
-  var hostedAppUrl = "https://cdn.jsdelivr.net/gh/bekabonsa/n/site/index.html";
+  var launcherBuild = "2026-04-01c";
+  var hostedAppUrl = "https://rawcdn.githack.com/bekabonsa/n/282ee0a/site/index.html";
   var tvInput = window.tizen && window.tizen.tvinputdevice;
   var buildNode = document.getElementById("launcher-build");
 
